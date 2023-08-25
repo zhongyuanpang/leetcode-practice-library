0c68c1927bf427413937e449c5796ec9d93c2012
pzy <pzy1013@163.com>
2023-08-25 14:32:03 +0800

    first commit

---
+++ src/main/java/com/leetcodepracticelibrary/code/LeetcodePracticeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.leetcodepracticelibrary.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodePracticeLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodePracticeLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leetcodepracticelibrary/code/LeetcodePracticeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcodepracticelibrary.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodePracticeLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
